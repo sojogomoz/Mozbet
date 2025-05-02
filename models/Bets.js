092f018b820c6bbaac60142d579dd17dcdc94eab
sojogomoz <sojogomoz@gmail.com>
2025-05-03 01:19:49 +0200

    Update Bets.js

--- models/Bets.js
+++ models/Bets.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
-// Definindo o esquema da aposta
-const betSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  valor: { type: Number, required: true },
-  escolha: { type: String, required: true },
-  dataAposta: { type: Date, default: Date.now }
+const BetSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  valor: {
+    type: Number,
+    required: true
+  },
+  escolha: {
+    type: String,
+    enum: ['cara', 'coroa'],
+    required: true
+  },
+  dataAposta: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model('Bet', betSchema);
+module.exports = mongoose.model('Bet', BetSchema);
